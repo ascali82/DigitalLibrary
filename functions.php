92e454bb8eb4a0befafe7bf33bd7820d1e2d6525
a.scali82 <alessandro.scali1982@gmail.com>
2018-09-08 16:14:26 +0200

    Boostrap 4, MDB and Font Awesome
    Add css/, font/, img/ and js/ folders with MDB archive.
    Create functions.php file with the first function that loads styles and scripts into the theme.

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Funzioni e definizioni del tema
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Digital Library
+ */
+
+/**
+ * Stili e script
+ */
+function theme_enqueue_scripts() {
+    // Stili - Caricati nella sezione head del tema
+        // Font Awesome 5.3.1
+        wp_enqueue_style( 'Font_Awesome', 'https://use.fontawesome.com/releases/v5.3.1/css/all.css' );
+        // Bootstrap 4
+        wp_enqueue_style( 'Bootstrap_css', get_template_directory_uri() . '/css/bootstrap.min.css' );
+        // MDB
+        wp_enqueue_style( 'MDB', get_template_directory_uri() . '/css/mdb.min.css' );
+        // Stili specifici del tema
+        wp_enqueue_style( 'Style', get_template_directory_uri() . '/style.css' );
+    // Script - Caricati nella sezione footer del tema
+        // jQuery
+        wp_enqueue_script( 'jQuery', get_template_directory_uri() . '/js/jquery-3.3.1.min.js', array(), '3.3.1', true );
+        // Popper
+        wp_enqueue_script( 'Tether', get_template_directory_uri() . '/js/popper.min.js', array(), '1.0.0', true );
+        // Bootstrap
+        wp_enqueue_script( 'Bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '1.0.0', true );
+        // MDB
+        wp_enqueue_script( 'MDB', get_template_directory_uri() . '/js/mdb.min.js', array(), '1.0.0', true );
+
+        }
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_scripts' );
+?>
