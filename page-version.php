17dfb7657a27eb864b7b50c7ed62afb440159836
a.scali82 <alessandro.scali1982@gmail.com>
2018-09-12 18:30:39 +0200

    Versioning
    Add version taxonomy in template-functions-php.
    Add page-version.php with taxonomy loop.

---
+++ page-version.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Template Name: Versioning Template
+ *
+ * @package _dl
+ */
+get_header();
+?>
+                <div id="wrapper" class="row d-flex flex-lg-row-reverse flex-column-reverse">
+
+                    <div id="primary" class="content-area col-12 col-lg-9">
+                        <main id="main" class="site-main">
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+        
+<?php 
+$terms_array = array( 
+  'taxonomy' => 'version', // you can change it according to your taxonomy
+  'parent'   => 0, // If parent => 0 is passed, only top-level terms will be returned
+    'orderby' =>  'name',
+  'order' =>  'DESC'
+);
+
+
+        
+$versions_terms = get_terms($terms_array); 
+foreach($versions_terms as $version): ?>
+<h2><?php echo $version->name; ?></h2>
+<?php 
+$post_args = array(
+      'posts_per_page' => -1,
+ //     'post_type' => 'service', // you can change it according to your custom post type
+      'tax_query' => array(
+          array(
+              'taxonomy' => 'version', // you can change it according to your taxonomy
+              'field' => 'term_id', // this can be 'term_id', 'slug' & 'name'
+              'terms' => $version->term_id,
+              'orderby' =>  'date',
+  'order' =>  'ASC'
+          )
+      )
+);
+$myposts = get_posts($post_args); ?>
+<div class="list-group">
+<?php foreach ( $myposts as $post ) : setup_postdata( $post ); ?>
+    <a href="<?php the_permalink(); ?>" class="list-group-item list-group-item-action"><?php the_title(); ?></a>
+<?php endforeach; // Term Post foreach ?>
+</div>
+<?php wp_reset_postdata(); ?>
+
+<?php endforeach; // End Term foreach; ?>
+        
+        
+	</div><!-- .entry-content -->
+    </article><!-- #post-<?php the_ID(); ?> -->
+                        </main><!-- #main -->
+                    </div><!-- #primary -->
+    
+<?php get_sidebar(); ?>
+
+                </div><!-- #wrapper -->
+<?php
+get_footer(); 
