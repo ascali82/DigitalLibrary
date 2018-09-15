6df599cbb96db4c0289e5c4e3a0a2cea04df0232
a.scali82 <alessandro.scali1982@gmail.com>
2018-09-15 17:07:38 +0200

    Front Page Style
    Add basic structure to front-page.php

--- front-page.php
+++ front-page.php
@@ -10,7 +10,101 @@ get_header();
 ?>
                 <div id="primary" class="content-area col-12">
                     <main id="main" class="site-main">
-                    
+ 
+      <div class="container marketing">
+
+        <!-- Three columns of text below the carousel -->
+        <div class="row">
+            <!--Panel-->
+<div class="card border-0 mb-3 col-12 p-0">
+    <div class="card-header bg-transparent">Ultimi Contenuti</div>
+    <div class="card-body text-primary d-inline-flex flex-wrap justify-content-space-evenly">
+<!--Panel-->
+<div class="card col-lg-3 col-sm-12 mx-auto mb-2">
+    <div class="card-body">
+        <h5 class="card-title">Post</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+
+    </div>
+            <div class="d-flex align-items-end flex-column">
+        <a href="#" class="btn btn-primary btn-md p-1">Vai alla Bacheca</a>
+        </div>
+</div>
+<!--/.Panel-->
+<!--Panel-->
+<div class="card col-lg-3 col-sm-12 mx-auto mb-2">
+    <div class="card-body">
+        <h5 class="card-title">Autori</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        
+    </div>
+                <div class="d-flex align-items-end flex-column">
+        <a href="#" class="btn btn-primary pull-right btn-md p-1">Vai al Catalogo Autori</a>
+        </div>    
+</div>
+<!--/.Panel-->
+<!--Panel-->
+<div class="card col-lg-3 col-sm-12 mx-auto mb-2">
+    <div class="card-body">
+        <h5 class="card-title">Opere</h5>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        
+    </div>
+            <div class="d-flex align-items-end flex-column">
+        <a href="#" class="btn btn-primary pull-right btn-md p-1">Vai al Catalogo Opere</a>
+        </div>    
+</div>
+<!--/.Panel-->         
+    </div>
+</div>
+<!--/.Panel-->
+
+        </div><!-- /.row -->
+
+
+        <!-- START THE FEATURETTES -->
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7 order-md-2">
+            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5 order-md-1">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <div class="row featurette">
+          <div class="col-md-7">
+            <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+          </div>
+          <div class="col-md-5">
+            <img class="featurette-image img-fluid mx-auto" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          </div>
+        </div>
+
+        <hr class="featurette-divider">
+
+        <!-- /END THE FEATURETTES -->
+
+      </div><!-- /.container -->                        
 
                     </main><!-- #main -->
                 </div><!-- #primary -->                    
